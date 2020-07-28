2263373be68f1d30250b9ad5a1f7e6d9bbb1adaa
auna <annah.ramones@gmail.com>
2020-07-28 01:21:41 -0700

    [annah] created ingredient and recipe models, populated components with a view of a list of recipes, as well as the items a recipe has. same for ingredients.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes:Recipe[] = [
+    new Recipe("The Best Noodles", "Tasty Noods", "https://thecozyapron.com/wp-content/uploads/2017/05/chilled-garlic-sesame-udon-noodles_thecozyapron_social.jpg"),
+    new Recipe("The Best Noodles", "Tasty Noods", "https://thecozyapron.com/wp-content/uploads/2017/05/chilled-garlic-sesame-udon-noodles_thecozyapron_social.jpg")
+  ];
 
   constructor() { }
 
